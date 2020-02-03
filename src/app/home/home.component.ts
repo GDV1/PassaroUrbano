79bcdda43e7e98453b6e87e94ff43cac8a2f3993
GDV1 <gabrieldv.dv@hotmail.com>
2020-02-02 23:40:09 -0300

    Create Ofertas Service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../shared/oferta.model';
+import { OfertasService } from '../ofertas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[];
+
+  constructor(
+    private ofertasService: OfertasService,
+  ) { }
 
   ngOnInit() {
+    this.ofertas = this.ofertasService.getOfertas();
+    console.log(this.ofertas);
   }
 
 }
