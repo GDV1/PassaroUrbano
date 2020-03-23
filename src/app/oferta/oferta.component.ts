7bd518ec74107af4da36abfbd21c9aaee029714d
GDV1 <gabrieldv.dv@hotmail.com>
2020-03-22 23:39:03 -0300

    Getting data with snapshot

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    console.log(this.route.snapshot.params['id']);
   }
 
 }
