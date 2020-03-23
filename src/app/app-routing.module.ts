442e04319b7c1268019adc180dd56c2369ba5861
GDV1 <gabrieldv.dv@hotmail.com>
2020-03-22 23:12:46 -0300

    Template adjustment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,8 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'restaurantes', component: RestaurantesComponent },
   { path: 'diversao', component: DiversaoComponent },
-  { path: 'oferta', component: OfertaComponent }
+  { path: 'oferta', component: OfertaComponent },
+  { path: 'oferta/:id', component: OfertaComponent }
 ];
 
 @NgModule({
