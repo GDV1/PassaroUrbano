a674914952f01027f048921e10d187b03751208f
GDV1 <gabrieldv.dv@hotmail.com>
2020-03-25 19:37:56 -0300

    Seting children routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { HomeComponent } from './home/home.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
+import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
+import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 
 
 const routes: Routes = [
@@ -12,7 +14,13 @@ const routes: Routes = [
   { path: 'restaurantes', component: RestaurantesComponent },
   { path: 'diversao', component: DiversaoComponent },
   { path: 'oferta', component: OfertaComponent },
-  { path: 'oferta/:id', component: OfertaComponent }
+  { path: 'oferta/:id', component: OfertaComponent,
+    children: [
+      {path: '', component: ComoUsarComponent},
+      {path: 'como-usar', component: ComoUsarComponent},
+      {path: 'onde-fica', component: OndeFicaComponent}
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -6,17 +6,27 @@ import { Oferta } from './shared/oferta.model';
 @Injectable()
 export class OfertasService {
 
+    private url_api = 'http://localhost:3000/ofertas?';
+
     constructor(private http: HttpClient) {}
 
     public getOfertas(): Promise<Oferta[]> {
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${this.url_api}destaque=true`)
         .toPromise()
         .then((resposta: any) => resposta);
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${this.url_api}categoria=${categoria}`)
         .toPromise()
         .then((resposta: any) => resposta);
+    }
+
+    public getOfertaPorId(id: number): Promise<Oferta> {
+        return this.http.get(`${this.url_api}categoria=${id}`)
+        .toPromise()
+        .then((resposta: any) => {
+            return resposta.shift();
+        });
     }
 }
