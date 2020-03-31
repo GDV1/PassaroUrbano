7656af51e678a3418c3275a5499f30bd702bcd30
GDV1 <gabrieldv.dv@hotmail.com>
2020-03-31 16:19:53 -0300

    Remove search field

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
 import { Oferta } from './shared/oferta.model';
-import { Observable } from 'rxjs';
 import { detalhesOferta } from './shared/detalhesOferta.model';
+import 'rxjs-compat/add/operator/map';
 
 @Injectable()
 export class OfertasService {
@@ -12,39 +11,33 @@ export class OfertasService {
 
     constructor(private http: HttpClient) {}
 
-    // Página Home - OK
+    // Página Home
     OfertasDestaque() {
         const destaque = `${this.urlBase}ofertas?destaque=true`;
         return this.http.get<Oferta[]>(destaque);
     }
 
-    // Página Restaurantes e Diversão - Consulta por categoria - OK
+    // Página Restaurantes e Diversão
     OfertasPorCategoria(categoria: string) {
         const ofertaPorCategoria = `${this.urlBase}ofertas?categoria=${categoria}`;
         return this.http.get<Oferta[]>(ofertaPorCategoria);
     }
 
-    // Página da Oferta - RETORNA MAS NÃO EXIBE
+    // Página da Oferta
     OfertasPorId(id: number) {
         const ofertaPorId = `${this.urlBase}ofertas?id=${id}`;
         return this.http.get<Oferta>(ofertaPorId);
     }
 
-    // Como Usar da Oferta por Id - RETORNA MAS NÃO EXIBE
+    // Como Usar da Oferta por Id
     ComoUsar(id: number) {
         const detalhesPorId = `${this.urlBase}como-usar?id=${id}`;
         return this.http.get<detalhesOferta>(detalhesPorId);
     }
 
-    // Onde Fica da Oferta por Id - RETORNA MAS NÃO EXIBE
+    // Onde Fica da Oferta por Id
     OndeFica(id: number) {
         const detalhesPorId = `${this.urlBase}onde-fica?id=${id}`;
         return this.http.get<detalhesOferta>(detalhesPorId);
     }
-
-    // Método do campo de pesquisa
-    // PesquisaOfertas(termo: string) {
-    //     return this.http.get(`${this.urlBase}ofertas?descricao_oferta=${termo}`)
-    //         .map(dados => console.log(dados));
-    // }
 }
--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,36 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { OfertasService } from '../ofertas.service';
-import { Observable, Subject } from 'rxjs';
-import { Oferta } from '../shared/oferta.model';
-
-import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
   styleUrls: ['./topo.component.css'],
-  providers: [ OfertasService ]
 })
 export class TopoComponent implements OnInit {
 
-  public ofertas;
-  private subjectPesquisa: Subject<string> = new Subject<string>();
+  constructor() { }
 
-  constructor(private ofertasService: OfertasService) { }
 
-  // public pesquisa(termoDaBusca: string): void {
-  //   console.log('Caracter: ', termoDaBusca);
-  //   this.subjectPesquisa.next(termoDaBusca);
-  // }
-
-  ngOnInit() {
-    // this.ofertas = this.subjectPesquisa
-    // .switchMap((termo: string) => {
-    //   console.log('Requisição http para a api');
-    //   return this.ofertasService.PesquisaOfertas(termo);
-    // });
-
-    // this.ofertas.subscribe((ofertas: Oferta[]) => console.log(ofertas));
-  }
+  ngOnInit() { }
 
 }
