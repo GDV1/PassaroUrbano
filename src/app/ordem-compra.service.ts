61b752de0564e988fb03982f4e5f5d8cbe4e2a61
GDV1 <gabrieldv.dv@hotmail.com>
2020-04-02 18:38:37 -0300

    Injecting the HttpClient

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,6 +1,13 @@
 import { Pedido } from './shared/pedido.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
+
+@Injectable()
 export class OrdemCompraService {
+
+    constructor(private http: HttpClient) {}
+
     public efetivarCompra(pedido: Pedido): void {
         console.log(pedido);
     }
