2902b2dbf0edfae4ec8b1036a10cf5f63eb9a3e7
GDV1 <gabrieldv.dv@hotmail.com>
2020-04-01 21:05:22 -0300

    Two-Way-Binding with form

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco = '';
+  public numero = '';
+  public complemento = '';
+  public formaPagamento = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public atualizaEndereco(endereco: string): void {
+    this.endereco = endereco;
+    console.log(this.endereco);
+  }
+
+  public atualizaNumero(numero: string): void {
+    this.numero = numero;
+    console.log(this.numero);
+  }
+
+  public atualizaComplemento(complemento: string): void {
+    this.complemento = complemento;
+    console.log(this.complemento);
+  }
+
+  public atualizaPagamento(pagamento: string): void {
+    this.formaPagamento = pagamento;
+    console.log(this.formaPagamento);
+  }
 }
