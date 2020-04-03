a3cbe218375b7f767a4db6577c0f4a56ab3f8d73
GDV1 <gabrieldv.dv@hotmail.com>
2020-04-03 00:21:22 -0300

    Linking component and template using reactive forms (FormGroup / FormControl)

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { OrdemCompraService } from '../ordem-compra.service'
-import { Pedido } from '../shared/pedido.model'
+import { OrdemCompraService } from '../ordem-compra.service';
+import { Pedido } from '../shared/pedido.model';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -10,11 +11,16 @@ import { Pedido } from '../shared/pedido.model'
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public formulario: FormGroup = new FormGroup({
+    'endereco': new FormControl(null),
+    'numero': new FormControl(null),
+    'complemento': new FormControl(null),
+    'formaPagamento': new FormControl(null),
+  });
+
   constructor(private ordemCompraService: OrdemCompraService) { }
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() { }
 
   public confirmarCompra(): void {
   }
