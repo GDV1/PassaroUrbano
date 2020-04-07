ac38e9249d8cf5115eee3a11b70d4f57687e8684
GDV1 <gabrieldv.dv@hotmail.com>
2020-04-06 23:24:23 -0300

    Sending form via HTTP POST request (Reactive Forms)

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -11,6 +11,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public idPedidoCompra: number = undefined;
+
   public formulario: FormGroup = new FormGroup({
     'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
     'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(10)]),
@@ -23,6 +25,18 @@ export class OrdemCompraComponent implements OnInit {
   ngOnInit() { }
 
   public confirmarCompra(): void {
+    let pedido: Pedido = new Pedido(
+      undefined,
+      this.formulario.value.endereco,
+      this.formulario.value.numero,
+      this.formulario.value.complemento,
+      this.formulario.value.formaPagamento
+    );
 
+    this.ordemCompraService.efetivarCompra(pedido)
+      .subscribe((idPedido: number) => {
+        this.idPedidoCompra = idPedido;
+        console.log(this.idPedidoCompra);
+      });
   }
 }
