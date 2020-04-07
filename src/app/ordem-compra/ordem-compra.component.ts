1c94244858939c915370654e64df7568571c55d6
GDV1 <gabrieldv.dv@hotmail.com>
2020-04-06 23:09:45 -0300

    Apply visual feedback and validations

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service';
 import { Pedido } from '../shared/pedido.model';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -12,10 +12,10 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class OrdemCompraComponent implements OnInit {
 
   public formulario: FormGroup = new FormGroup({
-    'endereco': new FormControl(null),
-    'numero': new FormControl(null),
+    'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(10)]),
     'complemento': new FormControl(null),
-    'formaPagamento': new FormControl(null),
+    'formaPagamento': new FormControl(null, [Validators.required]),
   });
 
   constructor(private ordemCompraService: OrdemCompraService) { }
@@ -23,5 +23,6 @@ export class OrdemCompraComponent implements OnInit {
   ngOnInit() { }
 
   public confirmarCompra(): void {
+
   }
 }
