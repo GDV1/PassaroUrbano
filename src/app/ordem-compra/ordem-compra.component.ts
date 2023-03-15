21930304b32cc1774e5ca6f84a4ac3546fb2bd2a
Gabriel Dias <gabrieldv.dv@hotmail.com>
2023-03-14 23:42:31 -0300

    Update for Angular 14

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,6 +1,6 @@
 // Default imports
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
 
 // Services
 import { CarrinhoService } from '../services/carinho.service';
@@ -21,11 +21,11 @@ export class OrdemCompraComponent implements OnInit {
   public idPedidoCompra: number = undefined;
   public itensCarrinho: ItemCarrinho[] = [];
 
-  public formulario: FormGroup = new FormGroup({
-    'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
-    'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(10)]),
-    'complemento': new FormControl(null),
-    'formaPagamento': new FormControl(null, [Validators.required]),
+  public formulario: UntypedFormGroup = new UntypedFormGroup({
+    'endereco': new UntypedFormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'numero': new UntypedFormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(10)]),
+    'complemento': new UntypedFormControl(null),
+    'formaPagamento': new UntypedFormControl(null, [Validators.required]),
   });
 
   constructor(
